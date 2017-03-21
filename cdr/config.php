9fe86fc828db2806a954683f70c5e4d0fb71caa4
typefo <typefo@qq.com>
2017-03-21 18:14:14 +0800

    add config.php file

---
+++ cdr/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * The mod json cdr request
+ * author: typefo
+ * e-mail: typefo@qq.com
+ */
+
+define('DB_HOST', '127.0.0.1');
+define('DB_PORT', 3306);
+define('DB_USER', 'root');
+define('DB_PASS', '123456');
+define('DB_NAME', 'bitcc');
--- cdr/public/cdr.php
+++ cdr/public/cdr.php
@@ -6,11 +6,8 @@
  * e-mail: typefo@qq.com
  */
 
-define('DB_HOST', '127.0.0.1');
-define('DB_PORT', 3306);
-define('DB_USER', 'root');
-define('DB_PASS', '123456');
-define('DB_NAME', 'bitcc');
+// load configure file
+require('../config.php');
 
 try {
 
