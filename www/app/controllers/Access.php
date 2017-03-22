a03af09b4ef52ea5fd81fbd995e0125cebabbe92
typefo <typefo@qq.com>
2017-03-22 16:05:32 +0800

    update html templete

---
+++ www/app/controllers/Access.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * The Access Controller
+ * Link http://github.com/typefo/pbx-mon
+ * By typefo <typefo@qq.com>
+ */
+
+class AccessController extends Yaf\Controller_Abstract {
+
+    public function statusAction() {
+        return true;
+	}
+}
+
+
---
+++ www/app/controllers/Gateway.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * The Gateway Controller
+ * Link http://github.com/typefo/pbx-mon
+ * By typefo <typefo@qq.com>
+ */
+
+class GatewayController extends Yaf\Controller_Abstract {
+
+    public function statusAction() {
+        return true;
+	}
+}
+
+
