8a011433f5dd74b11dbb81f7bc68a5f007cfdf40
typefo <typefo@qq.com>
2017-03-22 11:50:00 +0800

    update user controller

--- www/app/controllers/User.php
+++ www/app/controllers/User.php
@@ -29,7 +29,7 @@ class UserController extends Yaf\Controller_Abstract {
             }
         }
 
-	    $this->getView()->assign("message", $message);
+        $this->getView()->assign("message", $message);
         return true;
 	}
 }
