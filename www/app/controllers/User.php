437a470198eec796ad5f1ffe438ecf22ff4740e4
typefo <typefo@qq.com>
2017-03-22 11:48:51 +0800

    update user controller

--- www/app/controllers/User.php
+++ www/app/controllers/User.php
@@ -17,23 +17,21 @@ class UserController extends Yaf\Controller_Abstract {
 	        $newpassword = $request->getPost('newpassword');
 	        $user = new UserModel('admin');
             if ($user->changePassword($oldpassword, $newpassword)) {
-                $message = '<div class="alert alert-success alert-dismissible" style="text-align:center" role="alert">
-		               <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-			       <span aria-hidden="true">&times;</span></button>
-			       <strong>提示: </strong> 密码修改成功! </div>';
+                $message = '<div class="alert alert-success alert-dismissible" style="text-align:center" role="alert">'.
+                           '<button type="button" class="close" data-dismiss="alert" aria-label="Close">'.
+                           '<span aria-hidden="true">&times;</span></button>'.
+                           '<strong>提示: </strong> 密码修改成功! </div>';
             } else {
-                $message = '<div class="alert alert-warning alert-dismissible" style="text-align:center" role="alert">
-                               <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                               <span aria-hidden="true">&times;</span></button>
-                               <strong>警告: </strong> 密码修改失败，请检查原始密码是否正确，新密码长度必须大于 6 位 </div>';
+                $message = '<div class="alert alert-warning alert-dismissible" style="text-align:center" role="alert">'.
+                           '<button type="button" class="close" data-dismiss="alert" aria-label="Close">'.
+                           '<span aria-hidden="true">&times;</span></button>'.
+                           '<strong>警告: </strong> 密码修改失败，请检查原始密码是否正确，新密码长度必须大于 6 位 </div>';
             }
 	    }
 
 	    $this->getView()->assign("message", $message);
         return true;
 	}
-
-	
 }
 
 
