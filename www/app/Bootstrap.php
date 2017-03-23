a8516d77581a04d295183153a522df6e3ac650f4
typefo <typefo@qq.com>
2017-03-23 12:04:25 +0800

    update bootstrap file

--- www/app/Bootstrap.php
+++ www/app/Bootstrap.php
@@ -9,33 +9,32 @@
 class Bootstrap extends Yaf\Bootstrap_Abstract{
 
     public function _initConfig() {
-		$this->config = Yaf\Application::app()->getConfig();
-		Yaf\Registry::set('config', $this->config);
-	}
+        $this->config = Yaf\Application::app()->getConfig();
+        Yaf\Registry::set('config', $this->config);
+    }
 
-	public function _initDatabase() {
-	    $host = $this->config->db->host;
-	    $port = $this->config->db->port;
-	    $user = $this->config->db->user;
-	    $pass = $this->config->db->pass;
-	    $name = $this->config->db->name;
+    public function _initAuth(Yaf\Dispatcher $dispatcher) {
+        $authPlugin = new AuthPlugin();
+        $dispatcher->registerPlugin($authPlugin);
+    }
+    
+    public function _initDatabase() {
+        $host = $this->config->db->host;
+        $port = $this->config->db->port;
+        $user = $this->config->db->user;
+        $pass = $this->config->db->pass;
+        $name = $this->config->db->name;
 
-	    try {
-	        Yaf\Registry::set('db', new PDO('mysql:host=' . $host . ';port=' . $port . ';dbname=' . $name, $user, $pass));
-	    } catch (PDOException $e) {
-	        error_log($e->getMessage(), 0);
-	    }
+        try {
+            Yaf\Registry::set('db', new PDO('mysql:host=' . $host . ';port=' . $port . ';dbname=' . $name, $user, $pass));
+        } catch (PDOException $e) {
+            error_log($e->getMessage(), 0);
+        }
+    }
 
-	}
-
-	public function _initSession(Yaf\Dispatcher $dispatcher) {
-	    $session = Yaf\Session::getInstance();
-            Yaf\Registry::set('session', $session);
-            $session->start();
-	}
-
-	public function _initAuth(Yaf\Dispatcher $dispatcher) {
-		$authPlugin = new AuthPlugin();
-		$dispatcher->registerPlugin($authPlugin);
-	}
+    public function _initSession(Yaf\Dispatcher $dispatcher) {
+        $session = Yaf\Session::getInstance();
+        Yaf\Registry::set('session', $session);
+        $session->start();
+    }
 }
