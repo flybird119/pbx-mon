5a28bb882d56f1e8439e24450091faa0717f9c04
typefo <typefo@qq.com>
2017-03-26 11:59:50 +0800

    add report controller

---
+++ www/app/controllers/Report.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * The Report Controller
+ * Link http://github.com/typefo/pbx-mon
+ * By typefo <typefo@qq.com>
+ */
+
+class ReportController extends Yaf\Controller_Abstract {
+
+    public function indexAction() {
+        return true;
+	}
+}
+
+
