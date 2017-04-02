a30236ee79309e3f031e286087ce970b17762f1a
typefo <typefo@qq.com>
2017-04-02 19:17:02 +0800

    update report.sh file

--- script/report.sh
+++ script/report.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 DB='pbxmon'
+USER='root'
 PASS='123456'
 BEGIN=$(date +'%Y-%m-%d 08:00:00')
 END=$(date +'%Y-%m-%d 20:00:00')
 SQL="INSERT INTO report(value, create_time) SELECT count(id),now() FROM cdr WHERE create_time BETWEEN '${BEGIN}' AND '${END}'"
 
-/usr/bin/mysql -u root -p${PASS} ${DB} -e "${SQL}" &>> /tmp/report.log
+/usr/bin/mysql -u ${USER} -p${PASS} ${DB} -e "${SQL}" &>> /tmp/report.log
