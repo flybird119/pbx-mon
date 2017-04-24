2c65fe66c7ef5e5f6898fd01b5e28f4500cc0b68
typefo <typefo@qq.com>
2017-04-24 18:52:26 +0800

    updated some files

--- cdr/public/cdr.php
+++ cdr/public/cdr.php
@@ -10,32 +10,33 @@
 require('../config.php');
 
 try {
-
-    // Initialize mysql database
-    $db = new PDO('mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
-
     // get post json data
     $data = json_decode(file_get_contents('php://input'), true);
     if ($data) {
-        $rep = isset($data['variables']) ? $data['variables'] : '';
-        $rpf = isset($rep['rpf']) ? $rep['rpf'] : 'unknown';
-        $uuid = isset($rep['uuid']) ? $rep['uuid'] : 'unknown';
-        $src_ip = isset($rep['sip_network_ip']) ? $rep['sip_network_ip'] : '0.0.0.0';
-        $caller = isset($rep['sip_from_user']) ? $rep['sip_from_user'] : 'unknown';
-        $called = isset($rep['called']) ? $rep['called']：'unknown';
-        $duration = isset($rep['billsec'])) ? intval($rep['billsec']) : 0;
-        $file = date('Y/m/d/', intval($rep['start_epoch'])) . $caller . '-' . $called . '-' . $uuid . '.wav';
-        $create_time = isset($rep['start_stamp'])) ? urldecode($rep['start_stamp']) : '1970-01-01 08:00:00';
+    	// Initialize mysql connection
+    	$db = new PDO('mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+
+    	// Variable processing
+        $req = isset($data['variables']) ? $data['variables'] : '';
+        $rpf = isset($req['rpf']) ? $req['rpf'] : 'unknown';
+        $uuid = isset($req['uuid']) ? $req['uuid'] : 'unknown';
+        $src_ip = isset($req['sip_network_ip']) ? $req['sip_network_ip'] : '0.0.0.0';
+        $caller = isset($req['sip_from_user']) ? $req['sip_from_user'] : 'unknown';
+        $called = isset($req['called']) ? $req['called']：'unknown';
+        $duration = isset($req['billsec'])) ? intval($req['billsec']) : 0;
+        $file = date('Y/m/d/', intval($req['start_epoch'])) . $caller . '-' . $called . '-' . $uuid . '.wav';
+        $create_time = isset($req['start_stamp'])) ? urldecode($req['start_stamp']) : '1970-01-01 08:00:00';
 
         if ($duration > 0) {
             $db->query("INSERT INTO cdr(caller, called, duration, src_ip, rpf, file, create_time) values('$caller', '$called', $duration, '$src_ip', '$rpf', '$file', '$create_time')");
         }
+
+        // Close mysql connection
+    	$db = null;
     } else {
         error_log('php parse cdr application/json data failure', 0);
     }
 
-    // close mysql connection
-    $db = null;
 } catch (PDOException $e) {
     error_log($e->getMessage(), 0);
 }
